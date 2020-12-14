bab2d21bf3e75aed725b4078aabd3f59359ff296
Azhar Difa Arnanda <aazhard8@gmail.com>
2020-12-14 20:55:36 +0700

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text("Lab Ketujuh"),
+      ),
+      body: Center(
+        child: Text("Aplikasi Flutter - oleh Azhar Difa Arnanda"),
+      ),
+    ),
+  ));
+}
+
